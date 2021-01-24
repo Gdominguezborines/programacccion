b8e18487d005f9badb34bfc649f2ae48950d67b0
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2021-01-24 23:12:35 +0100

    1

---
+++ 2eva/src/Boletin14/src/main/java/boletin14/ConversorTemperaturas.java
@@ -0,0 +1,51 @@
+
+package boletin14;
+
+/**
+ * @author cambes
+ */
+public class ConversorTemperaturas {
+    
+   public static final float TemperatutaMinima=80F;        
+
+    public ConversorTemperaturas() {
+    }
+    
+   
+/**conversion de centigrados a farenheit y con throws le indico que la excepccion
+ * no va a ser capturada aqui sino pasada a try catch
+ * @throws TemperaturaErradaExcepcion 
+ */
+   
+      public float centigrAFarenh(float TempCenti)throws TemperaturaErradaExcepcion{
+       
+       if(TempCenti<TemperatutaMinima){
+               throw new TemperaturaErradaExcepcion("La temperatura tiene que ser mayor de 80º");// creo el mensaje que trasladare a mi excepccion 
+              
+          }else{
+         
+       //busque en google y es +32 no +32,4 par apasar a faranheit   (TempCenti*9/5)+32= a temperatura deseada
+           return(TempCenti*1.8F)+32F;
+   }
+   }
+      
+      public void centiAReamur(float gradosCels){
+          
+          try{ //En este metodo se captura la excepccion por que asi lo dice en el boletin
+              float TempRamur=gradosCels*0.8F;
+              
+              if(gradosCels<TemperatutaMinima){
+                  throw new TemperaturaErradaExcepcion("La temperatura tiene que ser mayor a 80º");// si salta la excepccion mostrara el mensaje
+                  }
+              
+              System.out.println("La Temperetura en grados celsius es de= "+gradosCels+"ºC en Reamur es de "+TempRamur);
+          
+              }catch (TemperaturaErradaExcepcion e){ // si saltara la excepccion se crea un nuebo objeto llamado e que usare abajo para:
+                  
+                    System.out.println(e.getMessage());//imprimir el mensaje que cree en la excepccion
+          }                   
+                 
+              
+          }          
+      }
+      
---
+++ 2eva/src/Boletin14/src/main/java/boletin14/LLamadaConversorTemperaturas.java
@@ -0,0 +1,62 @@
+/*	BOLETIN 14
+Nunha empresa utilizan uns aparellos que traballan a temperaturas expresadas en graos Fherenheit e graos reamur .Para axudalos, decidimos  facer unha aplicación “ conversor de temperaturas”.
+De xeito que unha persoa teclea a temperatura, en graos centigrados, e o conversor devolvenos a temperatura en graos Fharenheit e Reamur. Pero o aparello non pode traballar con temperaturas < 80 ºC .
+    Crea unha aplicación cas seguintes clases :
+•	A clase TemperaturaErradaExcepcion  que represente a nosa excepción ( recorda que a temperatura non pode ser < 80 ºC )
+•	A clase ConversorTemperaturas . Nesta clase :
+1-	Declara unha constante  co valor 80 ºC
+2-	Un método centigradosAFharenheit  que reciba unha temperatura, tipo float, en graos centígrados e calcula e retorne o valor correspondente en graos fharenheit 
+
+     º Fharenheir = 9,0 / 5, 0 * centígrados + 32, 4
+
+   Este método non tratará a excepción ( TemperaturaErradaExcepcion ), e, se a temperatura é < 80 º centígrados  lanzará a excepción que será capturada e tratada na clase principal . 
+
+3-Un método centígradosAReamur  que reciba unha temperatura, tipo float, en graos centígrados e calcula e non retorne o valor correspondente en graos Reamur .
+
+     º Reamur = 4,0 / 5,0 * centígrados 
+
+   Este método non tratara a excepción . A lanzará ao método de chamada
+*/
+package boletin14;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author cambes
+ */
+public class LLamadaConversorTemperaturas {
+
+    public static void main(String[] args) {
+      float respuesta;
+           
+        ConversorTemperaturas  Temp1 = new ConversorTemperaturas();
+        ConversorTemperaturas Temp2 = new ConversorTemperaturas();
+        
+        //ya que en el primer metodo no se captura la excepccion hay que hacerlo desde aqui
+        
+        //use do while para inicializar el programa tantas veces como se quiera sin tener que ejecutarlo
+        do {            
+    
+        
+        try {
+                float temp=Float.parseFloat(JOptionPane.showInputDialog("Introduce la temperatura en grados centrigados para comvertir a Farenheit"));
+           
+                 System.out.println("Los grados centigrados introducidos son "+temp+"ºC y en Farenheit son "+Temp1.centigrAFarenh(temp)+"F" );
+            
+            } catch (TemperaturaErradaExcepcion e) {
+                    System.out.println(e.getMessage());
+                   }
+        //capturo la segunda tempratura
+            float temp2=Float.parseFloat(JOptionPane.showInputDialog("Introduce la temperatura en grados centrigados para convertir a REamur"));
+       //llamo al metodo pasando la temperatura
+         Temp2.centiAReamur(temp2);
+      
+         //con esto saco si =0   no = 1 y asi se realizara el bucle o no
+               respuesta = JOptionPane.showConfirmDialog(null, "¿Quieres volver a ejecutar el conversor de temperatura?", "Elige tu opcion", JOptionPane.YES_NO_OPTION,3);
+        
+        } while (respuesta ==0);
+
+    
+    }
+    
+}
