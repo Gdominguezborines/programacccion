9d5f8dc260f424ce110717f8b14c068ae728031c
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2021-03-10 10:18:59 +0100

    1

--- 2eva/src/Boletin17/boletin17/Aplicaccion.java
+++ 2eva/src/Boletin17/boletin17/Aplicaccion.java
@@ -1,18 +1,18 @@
 /**
- * Realiza un programa para levar o mantemento dunha librería. Para iso :
-1.	Realiza un diagrama de clases que reflicta as relación entre as clases  e logo implementa o programa tendo en conta :
-Cada libro de levar : título , autor , ISBN (String ) , prezo , e  numero de unidades.
-O programa debe :
-•	engadir libros
-•	vender ( borrar ) libros 
-o	amosar  os libros que temos  ( Os libros deben estar ordenados polo campo titulo )
-•	dar de baixa os libros con numero de unidades =0
-•	consultar un libro determinado
-Nos métodos vender, amosar, baixas e consultar debes comprobar que o array recibido non é null. No caso contrario lanza una excepción.
-Para a clase aplicación utiliza un paquete e para as demais outro paquete.
-Na aplicación do programa utiliza un menú con todas as opción .
-
+ *  Realiza un programa para levar o mantemento dunha librería. Para iso :*
+ * 1.	Realiza un diagrama de clases que reflicta as relación entre as clases  e logo implementa o programa tendo en conta :
+ * Cada libro de levar : título , autor , ISBN (String ) , prezo , e  numero de unidades.
+ * O programa debe :
+ * •    engadir libros
+ * •	vender ( borrar ) libros
+ * o	amosar  os libros que temos  ( Os libros deben estar ordenados polo campo titulo )
+ * •	dar de baixa os libros con numero de unidades =0
+ * •	consultar un libro determinado
+ * Nos métodos vender, amosar, baixas e consultar debes comprobar que o array recibido non é null. No caso contrario lanza una excepción.
+ * Para a clase aplicación utiliza un paquete e para as demais outro paquete.
+ * Na aplicación do programa utiliza un menú con todas as opción .
  */
+
 package Boletin17.boletin17;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
