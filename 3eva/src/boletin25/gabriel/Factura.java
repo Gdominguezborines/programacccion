143774ac295f931d5440eb375931e074ae287886
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2021-05-04 19:36:42 +0200

    3eva

--- 3eva/src/boletin25/gabriel/Factura.java
+++ 3eva/src/boletin25/gabriel/Factura.java
@@ -1,6 +1,7 @@
 package com.gabriel;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  * @author cambes
@@ -9,7 +10,8 @@ public class Factura {
       
     
     public void Factura(){
-        ArrayList<Barco>  listaB = new ArrayList<>();
+    
+      ArrayList<Barco>  listaB = new ArrayList<>();
          
         EmbarcacionDeportiva obxED = new EmbarcacionDeportiva(250,12,"Pepon");
         Velero obxVe =new Velero(2,15,"El mejor");
@@ -19,7 +21,39 @@ public class Factura {
         listaB.add(obxVe);
         listaB.add(obxYa);
         
-        System.out.println(listaB);
+    
+     
+        int dias=0,precio=0,eslora=0;
+        String matricula="";
+         String[] botones = {"Embarcacion Deportiva", "Velero", "Yate"};
+    
+                int ventana = JOptionPane.showOptionDialog(null,"Pulsa un boton para selecionar tu embarcaccion:","Elige tu opcion:",0,3, null,botones, botones[0]);
+     
+            if(ventana == 0){ 
+                     dias=Integer.parseInt(JOptionPane.showInputDialog("Cuantos dias que quieres alquilar"));
+                     precio=((obxED.modulo()*dias)+(2*obxED.getPotencia()));
+                     matricula=obxED.getMatricula();
+                     eslora=obxED.getEslora();
+                   
+                }
+                else if(ventana == 1){
+                     dias=Integer.parseInt(JOptionPane.showInputDialog("Cuantos dias que quieres alquilar"));
+                     precio=((obxVe.modulo()*dias)+(obxVe.getMastiles()*8));
+                     matricula=obxVe.getMatricula();
+                     eslora=obxVe.getEslora();
+                }
+                else if(ventana == 2){
+                     dias=Integer.parseInt(JOptionPane.showInputDialog("Cuantos dias que quieres alquilar"));
+                     precio=((obxYa.modulo()*dias)+(2*obxYa.getPotencia()+(25*obxYa.getCamarotes())));
+                     matricula=obxYa.getMatricula();
+                     eslora=obxYa.getEslora();
+                }
+            
+            
+            
+            
+            JOptionPane.showMessageDialog(null,"Factura: " + "\n matricula: "+ matricula +"\n eslora: "+ eslora +" Metros"+ "\n precio: " + precio +" Euros" );
+        
     }
     
        
--- 3eva/src/boletin25/gabriel/Main.java
+++ 3eva/src/boletin25/gabriel/Main.java
@@ -1,37 +1,17 @@
 package com.gabriel;
-
-import java.util.ArrayList;
-import javax.swing.JOptionPane;
-
+ 
 
 /**
  * @author cambes
  */
 public class Main {
-   
-        public static void main(String[] args) {
-        
-        
-     Factura obx =new Factura();
-           
-       obx.Factura();
-        int dias;
-         String[] botones = {"Embarcacion Deportiva", "Velero", "Yate"};
     
-                int ventana = JOptionPane.showOptionDialog(null,"Pulsa un boton para selecionar tu embarcaccion:","Elige tu opcion:",0,3, null,botones, botones[0]);
      
-            if(ventana == 0){ 
-                     dias=Integer.parseInt(JOptionPane.showInputDialog("Cuantos dias que quieres alquilar"));
-                     
-                }
-                else if(ventana == 1){
-                     dias=Integer.parseInt(JOptionPane.showInputDialog("Cuantos dias que quieres alquilar"));
-               
-                }
-                else if(ventana == 2){
-                     dias=Integer.parseInt(JOptionPane.showInputDialog("Cuantos dias que quieres alquilar"));
-                
-                }
+    public static void main(String[] args) {
+        
+              Factura obxF = new Factura();
+              obxF.Factura();
+    
   
         }
             
